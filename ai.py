e7c1d7ef5d195f0d51b4d49c26e5ea2348631829
zm14744 <3338125799@qq.com>
2026-06-28 22:09:01 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,19 +1,25 @@
-from openai import OpenAI
+import requests
 
-client = OpenAI(
-    api_key="sk-0aaf311b073a419dbc352c02ef019b86",
-    base_url="https://api.deepseek.com"
-)
+API_KEY = "sk-0aaf311b073a419dbc352c02ef019b86"
+API_URL = "https://api.deepseek.com/v1/chat/completions"
 
-SYSTEM_PROMPT = "你是离散数学助手，回答要结构化"
+SYSTEM_PROMPT = "你是离散数学专业助手，回答要结构清晰。"
 
 def ask_ai(text):
-    response = client.chat.completions.create(
-        model="deepseek-chat",
-        messages=[
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "model": "deepseek-chat",
+        "messages": [
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": text}
         ]
-    )
+    }
 
-    return response.choices[0].message.content
+    res = requests.post(API_URL, headers=headers, json=data)
+    return res.json()["choices"][0]["message"]["content"]
+
